7798f4c51f8e0505737b0f858a3e0944aa6dc998
JonasQuebraScripts018 <gustavohpdelima@gmail.com>
2023-10-05 17:29:35 -0300

    ta bao

---
+++ src/main/resources/static/cadastroNotes.js
@@ -0,0 +1,20 @@
+function cadastrarNotebooks(){
+    let numero = $("#numero").val();
+    let patrimonio = $("#patrimonio").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/cadastro/notebooks",
+        data:{
+            numero: numero,
+            patrimonio: patrimonio,
+        },
+        success: function(data){
+            alert(data);
+        },
+        error: function(){
+            alert("deu ruim");
+        }
+    });
+
+}
---
+++ src/main/resources/static/home.js
@@ -0,0 +1,45 @@
+$('a').click(function(event){
+    event.preventDefault();
+    controleRotasGet($(this).attr("href"));
+});
+
+function controleRotasGet(url){
+    switch(url){
+        case "/logout":
+            gerarSwal(url);
+            break;
+        case "/cadastro/notebooks":
+            $.get(url, function(data){
+                $(".container").html(data);
+                $("#enviar").click(cadastrarNotebooks);
+            });
+
+                break;
+        default:
+            alert('cancelei o get e fiz nada mb');
+    }
+}
+
+function gerarSwal(urlSucesso){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success me-2',
+        cancelButton: 'btn btn-danger ms-2'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Sair?',
+      text: "Você realmente deseja sair da aplicação?",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: '<i class="fa-solid fa-thumbs-up"></i> Sim!',
+      cancelButtonText: '<i class="fa-solid fa-thumbs-down"></i> Não!',
+      reverseButtons: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href=urlSucesso;
+        }
+    });
+}
